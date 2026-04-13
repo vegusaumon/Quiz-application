44cdc20262016f708c71a59e596520a71078a164
vegusaumon <harkonnen124@gmail.com>
2026-04-13 03:36:13 -0400

    Second commit

---
+++ quiz_game.py
@@ -0,0 +1,147 @@
+from quiz_list import *
+import copy
+import random
+import time
+import threading
+import time
+from inputimeout import inputimeout, TimeoutOccurred
+# Create a quiz game to test how much you know about programming.
+# Prompt the user with multiple choice answers.
+# If the user gets a correct answer then add 1 to a count variable
+# To get the % of correct answers, divide the count by the length of questions
+# Use quiz questions from popular sites for python
+# At the end introduce the user to an answer sheet
+# If they want to check the answer sheet, it will have full explanations
+# SOURCE URL https://www.w3schools.com/quiztest/quiztest.asp?qtest=PYTHON
+
+
+# Return number of thread objects currently active
+
+
+
+
+""" 
+Core function handles the looping of all the questions in the list.
+Shuffles them for randomness so no questions are ever in the same order.
+Checks for the correct user input..
+
+Needs to add some robustness for error handling
+
+"""
+
+# Time limit of 30 seconds for the user to answer each question
+TIME_LIMIT = 30
+# List of valid integers to continue and refresh the timer.
+
+
+
+
+def main():
+    # Prompt the user for the difficulty of quiz they want to use.
+    difficulty = input("choose difficulty (Easy/Medium/Hard)").strip().lower()
+    if difficulty == "easy":
+        quiz_to_use = easy_quiz
+    elif difficulty == "medium":
+        quiz_to_use = medium_quiz
+    elif difficulty == "hard":
+        quiz_to_use = hard_quiz
+    else:
+        print("Invalid choice, defaulting to Easy")
+        quiz_to_use = easy_quiz
+
+
+    # Initialize a copy of the quiz, and a count. 
+    # We also shuffle the quiz copy to get a degree of random order.
+
+
+    quiz_copy = copy.deepcopy(quiz_to_use)
+    count = 0
+    random.shuffle(quiz_copy)
+    for q in quiz_copy:
+        random.shuffle(q["options"])
+        print("\n"+q["question"])
+
+        for i,option in enumerate(q["options"],1):
+            print(f"{i}.{option}")
+        
+        '''
+        Nested try block, the first nesting gets the input. 
+        If nothing is input in the time limit it will raise
+        Timeoutoccured and skip the question. Otherwise if we do an input
+        Then if that input is within the valid range then we accept it and
+        continue.
+
+        We need to add robustness so that questions aren't skipped.
+        
+        
+        '''
+        try:
+            user_input = inputimeout(
+                "Enter the number of your answer: ",
+                timeout=TIME_LIMIT
+
+            )
+
+            try:
+                user_input = int(user_input)
+
+                if not (1 <=user_input <= len(q["options"])):
+                    print("Invalid option number. Skipping question.")
+                    user_input = None
+            except ValueError:
+                print("Invalid input. Skipping question")
+                user_input = None
+
+        except TimeoutOccurred:
+            print("Too slow! Time's up! Skipping question.")
+            user_input = None
+            
+
+        
+        if user_input is not None:
+            selected_option = q["options"][int(user_input) -1 ]
+            if selected_option == q["answer"]:
+                count +=1
+    
+    print(f"\nFinal Score: {count}/{len(quiz_copy)}")
+
+
+
+    # Asks the user if they want to see the answer sheet
+    # This also needs to be formatted so it looks less ugly.
+    while True:
+        user_check  = input("Would you like to see the answer sheet (Y/N)").strip().upper()
+
+        if user_check == "Y":
+            for q in quiz_copy:
+                print("\n"+q["question"])
+                print("\n"+q["answer"])
+            break
+
+        elif user_check == "N":
+                print("Ending program")
+                break
+
+        else:
+            print("Please enter Y or N")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ quiz_list.py
@@ -0,0 +1,53 @@
+easy_quiz = [
+    {
+        "question": "What is the correct syntax to output 'Hello World' in Python",
+        "options": ["print('Hello World')",
+                    "echo('Hello World')",
+                    "echo 'Hello World'",
+                    "p('Hello World')"
+                    ],
+        "answer": "print('Hello World')"
+
+
+    },
+    {
+        "question": "How do you insert COMMENTS in Python code?",
+        "options": ["#This is a comment",
+                    "//This is a comment",
+                    "/*This is a comment*/"],
+        "answer": "#This is a comment"
+    },
+    {
+        "question": "Which one is NOT a legal variable name?",
+        "options": ["_myvar",
+                    "my_var",
+                    "my-var",
+                    "Myvawr"],
+        "answer": "my-var"
+    },
+    {
+        "question": "How do you create a variable with the numeric value 5?",
+        "options": ["Both the other answers are correct",
+                    "x = 5",
+                    "x = int(5)"],
+        "answer": "Both the other answers are correct"
+    },
+    {
+        "question": "What is the correct file extension for Python files",
+        "options": [".py",
+                    ".pt",
+                    ".pyth",
+                    ".pyt"],
+        "answer": ".py"
+    },
+    {
+        "question": "How do you create a variable with the floating number 2.8",
+        "options": ["Both the other answers are correct",
+                    "x = float(2.8)",
+                    "x = 2.8"],
+        "answer": "Both the other answers are correct"
+    }
+
+
+
+]
